dad04694fb7f655b03db79c89c3d208776a6706e
alvarogomez05 <alvatox11@gmail.com>
2025-01-29 14:19:24 +0100

    antes de empezar con los componentes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h1 class=" rounded p-2 text-center text-3xl text-yellow-500">Bienvenido al gestor de tareas</h1>
+                    <!-- {{ __("You're logged in!") }} -->
+
+                    <!-- {{Auth::user() -> tasks -> count()}} -->
+
+                    <!-- forEach para todas las tareas de un usuario -->
+                     @foreach (Auth::user() -> tasks as $task)
+
+                    <p class="mt-4 text-lg text-yellow-500 rounded p-2 ">
+                        {{ $task->title }}
+                    </p>
+                    <p>
+                        {{ $task->description }}
+
+                    </p>
+                    @endforeach
                 </div>
             </div>
         </div>
